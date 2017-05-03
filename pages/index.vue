084243cdb827c6139ba4e3ecf9d8443476250117
Neven Đuranec <neven@burza.hr>
2017-05-03 14:40:18 +0200

    Fix

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
     </div>
 </template>
 
-<style>
+<style lang="scss">
     .container {
         min-height: 100vh;
         display: flex;
@@ -16,7 +16,7 @@
     .title {
         font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
         display: block;
-        font-weight: 300;
+        font-weight: 700;
         font-size: 100px;
         color: #35495e;
         letter-spacing: 1px;
