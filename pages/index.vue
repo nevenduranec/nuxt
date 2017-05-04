48907d2ef7cfd35eb0eb7e2d438f47d7a6353a95
Neven Đuranec <neven@burza.hr>
2017-05-04 09:21:47 +0200

    Tweaks

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,14 @@
 <template>
-    <div class="container">
-        <h1 class="title">Hello!</h1>
+    <div class="Container">
+        <div class="Inner">
+            <h1 class="Title">Hi there!</h1>
+            <p>Say hello <a href="http://twitter.com/nevenduranec">@nevenduranec</a>.</p>
+        </div>
     </div>
 </template>
 
 <style lang="scss">
-    .container {
+    .Container {
         min-height: 100vh;
         display: flex;
         justify-content: center;
@@ -13,12 +16,15 @@
         text-align: center;
     }
 
-    .title {
+    .Title {
         font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-        display: block;
         font-weight: 700;
         font-size: 100px;
         color: #35495e;
         letter-spacing: 1px;
+    }
+
+    p {
+        font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial;
     }
 </style>
